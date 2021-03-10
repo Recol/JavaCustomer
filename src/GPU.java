dee2cded8f55e8b5b7ed6964446afff0f9062ed0
Deco <13683901+Recol@users.noreply.github.com>
2021-03-10 02:15:09 +0000

    Initial Skeleton Structure

---
+++ src/GPU.java
@@ -0,0 +1,8 @@
+
+
+public class GPU {
+	Engine object_engine = new Engine();
+	public void GPU_Items() {
+		
+	}
+}
---
+++ src/menu.java
@@ -0,0 +1,68 @@
+
+
+import java.util.Scanner;
+
+public class menu {
+
+	public static void main(String[] args) {
+		String option = "";
+		
+		/**
+		 * This will load in the necessary class objects which will be accessed via instantiation.
+		 */
+			
+			Engine engine_object = new Engine();
+			CPU object_CPU = new CPU();
+			GPU object_GPU = new GPU();
+			
+			Scanner input = new Scanner(System.in);
+			
+			do {
+				System.out.println("Menu");
+				identation();
+				
+				engine_object.Menu_Output();
+				option = input.nextLine();
+				switch(option) {
+				case "1":
+					/**
+					 * Load in the CPU class to load CPU entities which will be used for sale.
+					 */
+					object_CPU.CPU_Items();
+					
+					break;
+				case "2": 
+					/**
+					 * Load in the GPU class to load GPU entities which will be used for sale.
+					 */
+					object_GPU.GPU_Items();
+			break;
+			case "3":
+				/*
+				 * Load in all the classes which hold hardware data for option 3.
+				 */
+				object_CPU.CPU_Items();
+				object_GPU.GPU_Items();
+				break;
+			case "4": 
+				/**
+				 * Exit the program
+				 */
+				System.out.println("System now shutting down");
+				System.exit(0);
+				break;
+				}
+			}while (!option.equals("4"));
+			
+		}
+			public static void identation() {
+				System.out.println("");
+			/**
+			 * Used as an accessible whitespace for the menu.
+			 */
+			}
+			
+
+	}
+
+
