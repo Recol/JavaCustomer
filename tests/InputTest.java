754c8eef25f3de993f73facee0d357689c90b511
andrewgordonyule <andrewgordonyule@gmail.com>
2021-03-30 16:15:58 +0100

    Added getString() test for input

--- tests/InputTest.java
+++ tests/InputTest.java
@@ -4,12 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InputTest {
     @Test
-    public void getInt_returnsCorrectInt(){
+    public void getIntReturnsCorrectInt(){
         ByteArrayInputStream in = new ByteArrayInputStream("1".getBytes());
         System.setIn(in);
 
         int testFine = Input.getInt(0,5);
 
         assertEquals(1, testFine);
+    }
+
+    @Test
+    void getStringReturnsTrueWithString() {
+        ByteArrayInputStream in = new ByteArrayInputStream("test string".getBytes());
+        System.setIn(in);
+
+        String testString = Input.getString();
+
+        assertEquals("test string", testString);
     }
 }
